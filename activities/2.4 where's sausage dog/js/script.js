19a1ab604b45420570b92d7ed8d2e742e428ff59
ShapelessMaxi <maxime.perreault@hotmail.fr>
2022-01-13 16:43:30 -0500

    2.4 Activity: trying to create animals

--- activities/2.4 where's sausage dog/js/script.js
+++ activities/2.4 where's sausage dog/js/script.js
@@ -1,9 +1,41 @@
 "use strict";
 
-function preload() {}
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
 
-function setup() {
-  createCanvas();
+let animalImages = [];
+let animals = [];
+
+function preload() {
+  // load every images of animal
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    // store the image here
+    animalImages.push(animalImage);
+  }
 }
 
-function draw() {}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // create the animals
+  for (let i = 0; i < NUM_ANIMALS; i++) {
+    // choose a random location
+    let x = random(0, width);
+    let y = random(0, height);
+    // choose a random image
+    let animalImage = random(animalImages);
+    // create a new animal
+    let animal = new Animal(x, y, animalImages);
+    // store the animal here
+    animals.push(animal);
+  }
+}
+
+function draw() {
+  background(240, 240, 90);
+
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
+  }
+}
