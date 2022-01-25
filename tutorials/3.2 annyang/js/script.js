b07a107bf61a5c77818161bc7da6330558c17c37
ShapelessMaxi <maxime.perreault@hotmail.fr>
2022-01-24 20:27:15 -0500

    tut 3.2: started annyang video

--- tutorials/3.2 annyang/js/script.js
+++ tutorials/3.2 annyang/js/script.js
@@ -1,33 +1,7 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
   background(255, 255, 55);
 }
 
-
-/**
-Description of draw()
-*/
-function draw() {
-
-}
+function draw() {}
