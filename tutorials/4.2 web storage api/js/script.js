ba599aec33b0e168a57f7cf61c52c951f82a74d7
ShapelessMaxi <maxime.perreault@hotmail.fr>
2022-01-28 18:23:07 -0500

    tut 4.2: started web storage api video

--- tutorials/4.2 web storage api/js/script.js
+++ tutorials/4.2 web storage api/js/script.js
@@ -1,33 +1,9 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
+function preload() {}
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
   background(255, 255, 55);
 }
 
-
-/**
-Description of draw()
-*/
-function draw() {
-
-}
+function draw() {}
