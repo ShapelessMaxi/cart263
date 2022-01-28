adb941b3bb5b564d13132f4122d1af1914dc550e
ShapelessMaxi <maxime.perreault@hotmail.fr>
2022-01-28 18:20:48 -0500

    tut 4.1: finished json video

--- tutorials/4.1 json/js/script.js
+++ tutorials/4.1 json/js/script.js
@@ -1,9 +1,39 @@
 "use strict";
 
+let tarotData = undefined;
+let fortune = `nothing yet...`;
+
+function preload() {
+  tarotData = loadJSON(
+    `https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`
+  );
+}
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(windowWidth, 150);
+
+  let card = random(tarotData.tarot_interpretations);
+  fortune = random(card.fortune_telling);
 }
 
 function draw() {
-  background(0);
+  background(130, 130, 200);
+
+  push();
+  textSize(16);
+  textAlign(CENTER, CENTER);
+  fill(250, 250, 255);
+  text(fortune, width / 2, height / 2);
+  pop();
 }
+//
+// function mousePressed() {
+//   loadJSON(`assets/data/tarot_interpretations.json`, tarotLoaded);
+// }
+//
+// function tarotLoaded(data) {
+//   tarotData = data;
+//
+//   let card = random(tarotData.tarot_interpretations);
+//   fortune = random(card.fortune_telling);
+// }
