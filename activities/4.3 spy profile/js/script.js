89f8c3b4849f8952d33759fc4ca5fab4fea4d32d
ShapelessMaxi <maxime.perreault@hotmail.fr>
2022-01-28 19:01:06 -0500

    act 4.3: started spy activity

---
+++ activities/4.3 spy profile/js/script.js
@@ -0,0 +1,29 @@
+/**
+Spy Profile Generator
+Maxime Perreault
+
+~description of the activity~
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {}
+
+/**
+create the canvas
+
+*/
+function setup() {
+  createCanvas(500, windowHeight);
+}
+
+/**
+draw the background
+draw visual elements
+*/
+function draw() {
+  background(255, 255, 55);
+}
