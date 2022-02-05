e6aa83b1ba467fcbd941f5196211f3f7571516a5
ShapelessMaxi <maxime.perreault@hotmail.fr>
2022-02-05 16:41:29 -0500

    tut 5.3 : finished

--- tutorials/5.3 ml5.js handpose/js/script.js
+++ tutorials/5.3 ml5.js handpose/js/script.js
@@ -1,11 +1,11 @@
 /**
 
-ObjectDetector Framework
+Handpose Framework
 Pippin Barr
 
-A skeleton framework for using ml5.js's ObjectDetector feature. Includes a
-loading screen followed by a live webcam feed with all recognized objects
-outlined and labelled with a name and confidence rating.
+A skeleton framework for using ml5.js's Handpose feature. Includes a
+loading screen followed by a live webcam feed with a circle drawn at
+the tip of the user's index finger.
 
 */
 
@@ -16,14 +16,14 @@ let state = `loading`; // loading, running
 // User's webcam
 let video;
 // The name of our model
-let modelName = `CocoSsd`;
-// ObjectDetector object (using the name of the model for clarify)
-let cocossd;
-// The current set of predictions made by CocoSsd once it's running
+let modelName = `Handpose`;
+// Handpose object (using the name of the model for clarity)
+let handpose;
+// The current set of predictions made by Handpose once it's running
 let predictions = [];
 
 /**
-Starts the webcam and the ObjectDetector
+Starts the webcam and the Handpose
 */
 function setup() {
   createCanvas(640, 480);
@@ -32,31 +32,23 @@ function setup() {
   video = createCapture(VIDEO);
   video.hide();
 
-  // Start the CocoSsd model and when it's ready start detection
-  // and switch to the running state
-  cocossd = ml5.objectDetector("cocossd", {}, function () {
-    // Ask CocoSsd to start detecting objects, calls gotResults
-    // if it finds something
-    cocossd.detect(video, gotResults);
-    // Switch to the running state
-    state = `running`;
-  });
-}
+  // Start the Handpose model and switch to our running state when it loads
+  handpose = ml5.handpose(
+    video,
+    {
+      flipHorizontal: true,
+    },
+    function () {
+      // Switch to the running state
+      state = `running`;
+    }
+  );
 
-/**
-Called when CocoSsd has detected at least one object in the video feed
-*/
-function gotResults(err, results) {
-  // If there's an error, report it
-  if (err) {
-    console.error(err);
-  }
-  // Otherwise, save the results into our predictions array
-  else {
+  // Listen for prediction events from Handpose and store the results in our
+  // predictions array when they occur
+  handpose.on(`predict`, function (results) {
     predictions = results;
-  }
-  // Ask CocoSsd to detect objects again so it's continuous
-  cocossd.detect(video, gotResults);
+  });
 }
 
 /**
@@ -86,51 +78,34 @@ function loading() {
 
 /**
 Displays the webcam.
-If there are currently objects detected it outlines them and labels them
-with the name and confidence value.
+If there is a hand it outlines it and highlights the tip of the index finger
 */
 function running() {
-  // Display the webcam
-  image(video, 0, 0, width, height);
+  // Display the webcam with reveresd image so it's a mirror
+  let flippedVideo = ml5.flipImage(video);
+  image(flippedVideo, 0, 0, width, height);
 
   // Check if there currently predictions to display
-  if (predictions) {
-    // If so run through the array of predictions
-    for (let i = 0; i < predictions.length; i++) {
-      // Get the object predicted
-      let object = predictions[i];
-      if (object.label === `scissors`) {
-        push();
-        fill(0, 0, 0, 200);
-        rect(object.x, object.y, object.width, object.height);
-        pop();
-      }
-      // Highlight it on the canvas
-      highlightObject(object);
-    }
+  if (predictions.length > 0) {
+    // Technically there will only be ONE because it only detects ONE hand
+    // Get the hand predicted
+    let hand = predictions[0];
+    // Highlight it on the canvas
+    highlightHand(hand);
   }
 }
 
 /**
-Provided with a detected object it draws a box around it and includes its
-label and confidence value
+Provided with a detected hand it highlights the tip of the index finger
 */
-function highlightObject(object) {
-  // Display a box around it
+function highlightHand(hand) {
+  // Display a circle at the tip of the index finger
+  let index = hand.annotations.indexFinger[3];
+  let indexX = index[0];
+  let indexY = index[1];
   push();
-  noFill();
-  stroke(255, 255, 0);
-  rect(object.x, object.y, object.width, object.height);
-  pop();
-  // Display the label and confidence in the center of the box
-  push();
-  textSize(18);
   fill(255, 255, 0);
-  textAlign(CENTER, CENTER);
-  text(
-    `${object.label}, ${object.confidence.toFixed(2)}`,
-    object.x + object.width / 2,
-    object.y + object.height / 2
-  );
+  noStroke();
+  ellipse(indexX, indexY, 50);
   pop();
 }
