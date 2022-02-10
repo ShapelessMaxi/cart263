7dce02eec7e692550284bbee31fb121d2b168f36
ShapelessMaxi <maxime.perreault@hotmail.fr>
2022-02-10 17:01:32 -0500

    e4 : added start button

--- exercises/e4_bubblepopper/js/script.js
+++ exercises/e4_bubblepopper/js/script.js
@@ -44,6 +44,19 @@ let state = `intro`; // possible states : intro, game, end
 // refer to the cursor image
 let cursor = undefined;
 
+// refer to the start button
+let startButton = {
+  x: undefined,
+  y: undefined,
+  width: 200,
+  height: 50,
+  corner: 10,
+  text: `start`,
+  color1: { r: 200, g: 200, b: 100 },
+  color2: { r: 245, g: 245, b: 220 },
+  sliced: false,
+};
+
 /**
 create the canvas
 access the user's webcam
@@ -82,9 +95,6 @@ function setup() {
 
 /**
 checks which state need to be called
-draw the background
-draw the pin from the handpose data
-draw the bubble
 */
 function draw() {
   // takes care of choosing the state
@@ -95,34 +105,11 @@ function draw() {
   } else if (state === `end`) {
     end();
   }
-
-  // check if the bubble should pop
-  // let d = dist(tipX, tipY, bubble.x, bubble.y);
-  // if (d < bubble.size / 2) {
-  //   // reassign position if the bubble is popped
-  //   bubble.x = random(0, width);
-  //   bubble.y = height;
-  // }
-
-  // assign basic movement to the bubble
-  bubble.x += bubble.vx;
-  bubble.y += bubble.vy;
-
-  // reassign position if the bubble gets out of the canvas
-  if (bubble.y < 0) {
-    bubble.x = random(0, width);
-    bubble.y = height;
-  }
-
-  // draw the bubbles
-  push();
-  fill(0, 100, 200);
-  noStroke();
-  ellipse(bubble.x, bubble.y, bubble.size);
-  pop();
 }
 
 // predict the position of the index finger
+// call drawCursor()
+// call sliceButton()
 function handPosePredictions() {
   // check if there's a hand recognized
   if (predictions.length > 0) {
@@ -143,6 +130,9 @@ function handPosePredictions() {
 
     // draw the cursor
     drawCursor(baseX, baseY, tipX, tipY);
+
+    // store the tip of the finger here
+    sliceButton(tipX, tipY);
   }
 }
 
@@ -154,7 +144,7 @@ function drawCursor(x1, y1, x2, y2) {
   pop();
 }
 
-// takes care of all the intro state realted interactions
+// takes care of all the intro state related interactions
 function intro() {
   // draw the background
   background(207, 95, 143);
@@ -162,20 +152,102 @@ function intro() {
   // takes care of recognizing the user's index
   handPosePredictions();
 
+  // draw a start button to slice
+  drawStartButton();
+
   // draw a sword
   drawCursor();
 }
 
-// takes care of all the game state realted interactions
+// draw the start button
+function drawStartButton() {
+  // assign the position of the button
+  startButton.x = width / 2;
+  startButton.y = height / 1.3;
+
+  // draw a rectangle
+  push();
+  rectMode(CENTER);
+  fill(startButton.color1.r, startButton.color1.g, startButton.color1.b);
+  strokeWeight(5);
+  stroke(startButton.color2.r, startButton.color2.g, startButton.color2.b);
+  rect(
+    startButton.x,
+    startButton.y,
+    startButton.width,
+    startButton.height,
+    startButton.corner
+  );
+  pop();
+  // draw the text
+  push();
+  textAlign(CENTER, CENTER);
+  fill(startButton.color2.r, startButton.color2.g, startButton.color2.b);
+  textSize(24);
+  textStyle(BOLD);
+  text(startButton.text, startButton.x, startButton.y);
+  pop();
+
+  // draw the slice thingy
+  if (startButton.sliced) {
+    push();
+    rectMode(CENTER);
+    noStroke();
+    fill(207, 95, 143);
+    rect(startButton.x, startButton.y, 30, startButton.height + 10);
+    pop();
+  }
+}
+
+// takes care of slicing the button
+function sliceButton(tipX, tipY) {
+  // check if the distance of the tip is close enough too slice the button
+  let d = dist(tipX, tipY, startButton.x, startButton.y);
+  if (d < 100) {
+    // keeps track of the button being sliced
+    startButton.sliced = true;
+    // change the state of the game after 1 second
+    setTimeout(function () {
+      state = `game`;
+    }, 3000);
+  }
+}
+
+// takes care of all the game state related interactions
 function game() {
   // draw the background
   background(118, 142, 181);
 
   // takes care of recognizing the user's index
   handPosePredictions();
+
+  // draw the bubbles
+  push();
+  fill(0, 100, 200);
+  noStroke();
+  ellipse(bubble.x, bubble.y, bubble.size);
+  pop();
+
+  // // assign basic movement to the bubble
+  // bubble.x += bubble.vx;
+  // bubble.y += bubble.vy;
+  //
+  // // reassign position if the bubble gets out of the canvas
+  // if (bubble.y < 0) {
+  //   bubble.x = random(0, width);
+  //   bubble.y = height;
+  // }
+
+  // check if the bubble should pop
+  // let d = dist(tipX, tipY, bubble.x, bubble.y);
+  // if (d < bubble.size / 2) {
+  //   // reassign position if the bubble is popped
+  //   bubble.x = random(0, width);
+  //   bubble.y = height;
+  // }
 }
 
-// takes care of all the end state realted interactions
+// takes care of all the end state related interactions
 function end() {
   // draw the background
   background(24, 15, 36);
