af48beb1d724307549f4566707e24a9f7a332922
ShapelessMaxi <maxime.perreault@hotmail.fr>
2022-02-10 16:28:26 -0500

    e4 : added a sword cursor

--- exercises/e4_bubblepopper/js/script.js
+++ exercises/e4_bubblepopper/js/script.js
@@ -20,6 +20,8 @@ new plan:
 2- create a bubble class so its easier to create more at the same time
 3- add different stuff that bubble can do?
 4- add counter of bubbles popped
+- make it look better :)))))
+
 */
 
 "use strict";
@@ -37,11 +39,15 @@ let predictions = [];
 let bubble = undefined;
 
 // refer to the different states
-let states = `intro`; // possible states : intro, game, end
+let state = `intro`; // possible states : intro, game, end
+
+// refer to the cursor image
+let cursor = undefined;
 
 /**
 create the canvas
 access the user's webcam
+load the cursor image
 */
 function setup() {
   // create a canvas
@@ -58,7 +64,6 @@ function setup() {
 
   // listen for predictions
   handpose.on(`predict`, function (results) {
-    console.log(results);
     predictions = results;
   });
 
@@ -70,17 +75,55 @@ function setup() {
     vx: 0,
     vy: -1.8,
   };
+
+  // load the cursor image
+  cursor = loadImage(`assets/images/sword.png`);
 }
 
 /**
+checks which state need to be called
 draw the background
 draw the pin from the handpose data
 draw the bubble
 */
 function draw() {
-  // draw the background
-  background(10, 10, 10);
+  // takes care of choosing the state
+  if (state === `intro`) {
+    intro();
+  } else if (state === `game`) {
+    game();
+  } else if (state === `end`) {
+    end();
+  }
 
+  // check if the bubble should pop
+  // let d = dist(tipX, tipY, bubble.x, bubble.y);
+  // if (d < bubble.size / 2) {
+  //   // reassign position if the bubble is popped
+  //   bubble.x = random(0, width);
+  //   bubble.y = height;
+  // }
+
+  // assign basic movement to the bubble
+  bubble.x += bubble.vx;
+  bubble.y += bubble.vy;
+
+  // reassign position if the bubble gets out of the canvas
+  if (bubble.y < 0) {
+    bubble.x = random(0, width);
+    bubble.y = height;
+  }
+
+  // draw the bubbles
+  push();
+  fill(0, 100, 200);
+  noStroke();
+  ellipse(bubble.x, bubble.y, bubble.size);
+  pop();
+}
+
+// predict the position of the index finger
+function handPosePredictions() {
   // check if there's a hand recognized
   if (predictions.length > 0) {
     // get the hand data
@@ -98,43 +141,42 @@ function draw() {
     let baseX = base[0];
     let baseY = base[1];
 
-    // draw a pin
-    push();
-    noFill();
-    stroke(255, 255, 255);
-    strokeWeight(2);
-    line(baseX, baseY, tipX, tipY);
-    pop();
-    // draw the base of the pin
-    push();
-    noStroke();
-    fill(255, 0, 0);
-    ellipse(baseX, baseY, 20);
-    pop();
-
-    // check if the bubble should pop
-    let d = dist(tipX, tipY, bubble.x, bubble.y);
-    if (d < bubble.size / 2) {
-      // reassign position if the bubble is popped
-      bubble.x = random(0, width);
-      bubble.y = height;
-    }
+    // draw the cursor
+    drawCursor(baseX, baseY, tipX, tipY);
   }
+}
 
-  // assign basic movement to the bubble
-  bubble.x += bubble.vx;
-  bubble.y += bubble.vy;
-
-  // reassign position if the bubble gets out of the canvas
-  if (bubble.y < 0) {
-    bubble.x = random(0, width);
-    bubble.y = height;
-  }
-
-  // draw the bubble
+// draw the cursor according to the user's hand's postion
+function drawCursor(x1, y1, x2, y2) {
   push();
-  fill(0, 100, 200);
-  noStroke();
-  ellipse(bubble.x, bubble.y, bubble.size);
+  imageMode(CORNER);
+  image(cursor, x1, y1, x2, y2);
   pop();
+}
+
+// takes care of all the intro state realted interactions
+function intro() {
+  // draw the background
+  background(207, 95, 143);
+
+  // takes care of recognizing the user's index
+  handPosePredictions();
+
+  // draw a sword
+  drawCursor();
+}
+
+// takes care of all the game state realted interactions
+function game() {
+  // draw the background
+  background(118, 142, 181);
+
+  // takes care of recognizing the user's index
+  handPosePredictions();
+}
+
+// takes care of all the end state realted interactions
+function end() {
+  // draw the background
+  background(24, 15, 36);
 }
