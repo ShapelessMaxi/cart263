17a1085c586a1db9f98b1048151b00aa240000da
ShapelessMaxi <maxime.perreault@hotmail.fr>
2022-02-10 14:06:13 -0500

    e4 : wrote a plan

--- exercises/e4_bubblepopper/js/script.js
+++ exercises/e4_bubblepopper/js/script.js
@@ -3,6 +3,17 @@ Bubble Popper
 Maxime Perreault
 
 pop bubbles with finger :o
+(maybe not 'pop', but like pet ur forehead?)
+- 1st state : setup/intro
+- ask the user to 'strike a pose' and to say 'aaaah'
+    - save an img of the user
+    (or maybe we should detect the 'person' with ml5.js ObjectDetector and relocate it later on?)
+    - save a sound file of the user
+- 2nd state : game
+- use the img of the user (or the detection of the users face?) as the head of a stick figure
+- use the sound of the user when the head is touched
+    - process in a few different ways
+- add loading screen
 */
 
 "use strict";
